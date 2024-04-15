6f656be44c7064a000990b089de275c2c514505b
Kotaro Inoue <k.musaino@gmail.com>
2024-04-16 03:36:34 +0900

    Double baset64 encode 1password credential
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- bootstrap/apply_secrets.sh
+++ bootstrap/apply_secrets.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-kubectl create secret generic -n kube-system op-credentials --from-literal=1password-credentials.json=$(op document get  --vault Kubernetes "Homemofu Credentials File")
-kubectl create secret generic -n kube-system onepassword-connect-token-kubernetes --from-literal=token=$(op item get --vault Kubernetes "Homemofu Access Token: infra-cluster" --field "credential")
+kubectl create secret generic -n kube-system op-credentials --from-literal=1password-credentials.json=$(op document get  --vault Kubernetes "Homemofu Credentials File"|base64 -w 0) --dry-run=client -o yaml | kubectl apply -f -
+kubectl create secret generic -n kube-system onepassword-connect-token-kubernetes --from-literal=token=$(op item get --vault Kubernetes "Homemofu Access Token: infra-cluster" --field "credential") --dry-run=client -o yaml | kubectl apply -f -
